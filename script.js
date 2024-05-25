0208a3266d3764b2a7229d46dca4ccaa9d8380f3
Bitar9 <131416101+Bitar9@users.noreply.github.com>
2024-05-25 09:18:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,43 +2,43 @@ document.addEventListener('DOMContentLoaded', () => {
     const certifications = [
         {
             title: "Data Science & Machine Learning",
-            url: "https://programs.edraak.org/learn/course/ds100-v2019sp/issue_certificate/?lang=en",
+            url: "certificates/Data_Science_Machine_Learning.pdf",
             issuer: "Edraak.org",
             date: "2020-05-10"
         },
         {
             title: "Protect Systems from Penetrations",
-            url: "https://programs.edraak.org/learn/course/kl-vsp-2019/issue_certificate/?lang=en",
+            url: "certificates/Protect_Systems_from_Penetrations.pdf",
             issuer: "Edraak.org",
             date: "2021-06-15"
         },
         {
             title: "Computer Essentials",
-            url: "https://programs.edraak.org/learn/course/icdl1-v2019sp/issue_certificate/?lang=en",
+            url: "certificates/Computer_Essentials.pdf",
             issuer: "Edraak.org",
             date: "2019-08-20"
         },
         {
             title: "Advanced Excel - (2)",
-            url: "https://programs.edraak.org/learn/course/ad104-vt1-2021/issue_certificate/?lang=en",
+            url: "certificates/Advanced_Excel_2.pdf",
             issuer: "Edraak.org",
             date: "2021-10-05"
         },
         {
             title: "Data Analysis with Python",
-            url: "https://courses.cognitiveclass.ai/certificates/6850ae965ffc47308b40fd3d8cf0bb9e",
+            url: "certificates/Data_Analysis_with_Python.pdf",
             issuer: "cognitiveclass.ai",
             date: "2022-03-18"
         },
         {
             title: "Emotional Intelligence",
-            url: "https://programs.edraak.org/learn/course/ei-vt2_2018/issue_certificate/?lang=en",
+            url: "certificates/Emotional_Intelligence.pdf",
             issuer: "Edraak.org",
             date: "2020-11-22"
         },
         {
             title: "The Upper-Intermediate Level (B2)",
-            url: "https://programs.edraak.org/learn/course/b2-vb2/issue_certificate/?lang=en",
+            url: "certificates/Upper_Intermediate_Level_B2.pdf",
             issuer: "Edraak.org",
             date: "2018-12-12"
         }
