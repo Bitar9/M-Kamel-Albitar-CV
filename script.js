e67182e75f6225412328bd1ad3dacd0ad711f204
Bitar9 <131416101+Bitar9@users.noreply.github.com>
2024-08-19 21:40:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
             title: "CS50",
             issuer: "Harvard.edu",
             date: "August 2024",
-            url: "CS50.pdf",
+            url: "Cs50.pdf",
             logo: "harvard_logo.png"
         },
         // More certifications here
